934aa9c52d0092030fc1a859d4648a15ce267e1e
robertelias <robert.elias@live.com>
2021-02-19 12:20:24 -0800

    Finalized Roster Application

--- Roster/main.py
+++ Roster/main.py
@@ -16,9 +16,33 @@ player = input("Who is your center: ").title()
 roster.append(player)
 #Dispaly Roster
 print("\n\tYour starting 5 for the upcoming basketball season")
-print("\T\TPoint Guard:\t\t" + roster[0])
-print("\T\TShooting Guard:\t\t" + roster[1])
-print("\T\TSmall Forward:\t\t" + roster[2])
-print("\T\TPower Forward:\t\t" + roster[3])
-print("\T\TCenter:\t\t" + roster[4])
+print("\t\tPoint Guard:\t\t" + roster[0])
+print("\t\tShooting Guard:\t\t" + roster[1])
+print("\t\tSmall Forward:\t\t" + roster[2])
+print("\t\tPower Forward:\t\t" + roster[3])
+print("\t\tCenter:\t\t\t" + roster[4])
 print(roster)
+
+#Remove an injured player
+injured_player = roster.pop(2)
+print("\nOh no, " + injured_player + " is injured...")
+roster_length = len(roster)
+print("Your roster only has " + str(roster_length) + " players.")
+
+#Add a player
+added_player = input("Who will take " + injured_player + "'s spot: ").title()
+roster.insert(2,added_player)
+
+
+#Dispaly Roster
+print("\n\tYour starting 5 for the upcoming basketball season")
+print("\t\tPoint Guard:\t\t" + roster[0])
+print("\t\tShooting Guard:\t\t" + roster[1])
+print("\t\tSmall Forward:\t\t" + roster[2])
+print("\t\tPower Forward:\t\t" + roster[3])
+print("\t\tCenter:\t\t\t" + roster[4])
+
+print("\nGood luck " + roster[2] + " you will do great!")
+roster_length = len(roster)
+print("Your roster now has " + str(roster_length) + " players.")
+# print(roster)
