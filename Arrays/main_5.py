0b90dd105bc12dae00e27217c1381f8974782354
robertelias <robert.elias@live.com>
2021-03-09 09:36:22 -0800

    array memory

---
+++ Arrays/main_5.py
@@ -0,0 +1,10 @@
+#5. Write a Python program to get the current memory address
+
+from array import *
+array_num = array('i', [1,3,5,7,9,12])
+print("Original array : "+str(array_num))
+
+print("Current memory address and the length in elements of the buffer: " +
+      str(array_num.buffer_info()))
+print("The size of the memory buffer in bytes: " +
+      str(array_num.buffer_info()[1] * array_num.itemsize))
